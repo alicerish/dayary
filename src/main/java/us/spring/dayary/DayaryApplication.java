506bb32ba4b4aa29fbb505bce0ee7cde052dea61
otgit <otgit@yahoo.com>
2020-10-31 12:47:51 +0900

    Initial commit

---
+++ src/main/java/us/spring/dayary/DayaryApplication.java
@@ -0,0 +1,13 @@
+package us.spring.dayary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/spring/dayary/DayaryApplicationTests.java
@@ -0,0 +1,13 @@
+package us.spring.dayary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
