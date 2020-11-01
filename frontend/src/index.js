c383d0dd9368abc487ba3c313aecc38c1e679203
yzzeee <goooooodgood@gmail.com>
2020-11-02 00:24:27 +0900

    + React init setting.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,5 @@
-// import React, { Component } from "react";
-// import ReactDOM from 'react-dom';
-//
-// class Main extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Demo Component</h1>
-//                 <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
-//             </div>
-//         );
-//     }
-// }
-//
-// ReactDOM.render(
-//     <Main />,
-//     document.getElementById('react-mountpoint')
-// );
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-console.log("Hello React");
+ReactDOM.render(<App />, document.getElementById("root"));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,41 @@
-var path = require("path");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-module.exports = {
-    output : {
-        filename: "[name].bundle.js",
-        path: __dirname + "/build"
-    },
-    devServer: {
-        contentBase: __dirname + "/build/",
-        inline: true,
-        hot: true,
-        host: "localhost",
-        port: 3000
-    },
-}
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: '/',
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer : {
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      // template: path.resolve(__dirname, 'public', 'index.html'),
+      template: "public/index.html", //source html
+    }),
+  ],
+};
+
+module.exports = config;
